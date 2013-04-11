6f2e88989871939b0d2b4db6276e6d6c65ec47c5
Dan Bentley <danbentley9@gmail.com>
2013-04-11 16:23:02 +0100

    Replacing x and y with ballPosition object

--- js/game.js
+++ js/game.js
@@ -21,7 +21,7 @@ function draw() {
     ctx.fillStyle = backcolor;
     clear();
     ctx.fillStyle = ballcolor;
-    circle(x, y, ballRadius);
+    circle(ballPosition.x, ballPosition.y, ballRadius);
 
     paddlePosition = {
         x: paddlex + PADDLE_POSITION_OFFSET.x,
@@ -35,34 +35,34 @@ function draw() {
     // http://www.harveycartel.org/metanet/tutorials/tutorialA.html
     rowheight = BRICKHEIGHT + PADDING;
     colwidth = BRICKWIDTH + PADDING;
-    row = Math.floor(y/rowheight);
-    col = Math.floor(x/colwidth);
+    row = Math.floor(ballPosition.y/rowheight);
+    col = Math.floor(ballPosition.x/colwidth);
     //reverse the ball and mark the brick as broken
-    if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
+    if (ballPosition.y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] == 1) {
         tempBallPosition.y = -tempBallPosition.y;
         bricks[row][col] = 0;
     }
 
     // If the ball has hit the wall
-    if (x + tempBallPosition.x + ballRadius > WIDTH || x + tempBallPosition.x - ballRadius < 0) {
+    if (ballPosition.x + tempBallPosition.x + ballRadius > WIDTH || ballPosition.x + tempBallPosition.x - ballRadius < 0) {
         tempBallPosition.x = -tempBallPosition.x;
     }
 
-    if (y + tempBallPosition.y - ballRadius < 0) {
+    if (ballPosition.y + tempBallPosition.y - ballRadius < 0) {
         tempBallPosition.y = -tempBallPosition.y;
-    } else if (y + tempBallPosition.y + ballRadius > HEIGHT - paddleh + PADDLE_POSITION_OFFSET.y) {
+    } else if (ballPosition.y + tempBallPosition.y + ballRadius > HEIGHT - paddleh + PADDLE_POSITION_OFFSET.y) {
         // Ball has hit paddle
-        if (x > paddlePosition.x && x < paddlePosition.y) {
+        if (ballPosition.x > paddlePosition.x && ballPosition.x < paddlePosition.y) {
             //move the ball differently based on where it hit the paddle
-            tempBallPosition.x = 8 * ((x-(paddlex+paddlew/2))/paddlew);
+            tempBallPosition.x = 8 * ((ballPosition.x-(paddlex+paddlew/2))/paddlew);
             tempBallPosition.y = -tempBallPosition.y;
-        } else if (y + tempBallPosition.y + ballRadius > HEIGHT) {
+        } else if (ballPosition.y + tempBallPosition.y + ballRadius > HEIGHT) {
             clearInterval(intervalId);
         }
     }
 
-    x += tempBallPosition.x;
-    y += tempBallPosition.y;
+    ballPosition.x += tempBallPosition.x;
+    ballPosition.y += tempBallPosition.y;
 }
 
 function drawPaddle() {
