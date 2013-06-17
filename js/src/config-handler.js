d376e2b874f97bd1d3c56f207cdfd5471bc9b358
Dan Bentley <danbentley9@gmail.com>
2013-06-17 12:14:44 +0100

    Adding selects to change ball radius, speed and paddle width

--- js/src/config-handler.js
+++ js/src/config-handler.js
@@ -1,3 +1,24 @@
-define(['paddle'], function(paddle) {
-      
+define(['jquery', 'paddle', 'ball', 'game'], function($, paddle, ball, game) {
+
+    $(document).ready(function() {
+        $('#paddle-width').on('change', function(e) {
+            paddle.width = $(this).val();
+        });
+
+        $('#ball-radius').on('change', function(e) {
+            ball.radius = $(this).val() / 10;
+        });
+
+        $('#ball-speed').on('change', function(e) {
+            ball.velocity = {
+                x: +$(this).val(),
+                y: $(this).val() * -1
+            };
+            console.log(ball);
+        });
+
+        $('#canvas').on('click', function(e) {
+            game.pause();
+        });
+    });
 });
